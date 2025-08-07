0db13867311e04b2c4dab818912168d79a324a7a
NisargKaneriya-Prioxis <Nisarg@prioxis.com>
2025-08-07 16:46:15 +0530

    Initial Commit

--- DMS.Models/obj/Debug/net8.0/DMS.Models.AssemblyInfo.cs
+++ DMS.Models/obj/Debug/net8.0/DMS.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMS.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a07ab4c48837e504d8835a29d04f2c5f0b0e4bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2849a0fe82c30bc43861227cb665a88e07f79e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMS.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMS.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
