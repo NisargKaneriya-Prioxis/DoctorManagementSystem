5a07ab4c48837e504d8835a29d04f2c5f0b0e4bb
NisargKaneriya-Prioxis <Nisarg@prioxis.com>
2025-08-07 16:08:12 +0530

    Initial Commit

--- DMS.Services/obj/Debug/net8.0/DMS.Services.AssemblyInfo.cs
+++ DMS.Services/obj/Debug/net8.0/DMS.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMS.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccaf2a9d0b89e4f852c3b0879e5368a0fa628bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365b2fff2f9fe709397bc414f95e53095e72401f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMS.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMS.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
