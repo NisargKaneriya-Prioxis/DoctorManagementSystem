d053c269258c998ee90e1474a5f6875a9a4e49d5
NisargKaneriya-Prioxis <Nisarg@prioxis.com>
2025-08-08 09:37:52 +0530

    Initial commit - cleaned with .gitignore

--- DMS.Common/obj/Debug/net8.0/DMS.Common.AssemblyInfo.cs
+++ DMS.Common/obj/Debug/net8.0/DMS.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DMS.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2849a0fe82c30bc43861227cb665a88e07f79e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db13867311e04b2c4dab818912168d79a324a7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DMS.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DMS.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
